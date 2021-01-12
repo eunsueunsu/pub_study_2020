d5be2ed13d205eaa61b43ebb4046113685d09d7a
eunsueunsu <imimim93@gmail.com>
2021-01-12 17:31:52 +0900

    watcha pedia first commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React from "react"
 import {BrowserRouter as Router, Route,  Switch} from "react-router-dom"
 import Watcha from "./page/WatchaLoginPage"
+import WatchaPediaMain from "./page/WatchaPedia/main";
 
 const Routes = () => {
     return( <Router>
             <Switch>
                 <Route path ="/watcha" component={Watcha}/>
-
+                <Route path="/watcha_pedia" component={WatchaPediaMain}/>
             </Switch>
         </Router>
     )
